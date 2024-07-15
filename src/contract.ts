2a1cb3512526a9dd150c902f208f13faa341298b
ethdev279 <45661693+ethdev279@users.noreply.github.com>
2024-07-15 23:38:57 +0530

    WIP: index transaction logs, decoding log data

--- src/contract.ts
+++ src/contract.ts
@@ -1,17 +1,62 @@
-import { Withdraw as WithdrawEvent } from "../generated/Contract/Contract"
-import { Withdraw } from "../generated/schema"
+import { BigInt, log, Address, Bytes, ethereum } from "@graphprotocol/graph-ts";
+import { Withdraw as WithdrawEvent } from "../generated/Contract/Contract";
+import { Withdraw, Transfer } from "../generated/schema";
+
+const TRANSFER_EVENT_SIGNATURE =
+  "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+
+// Function to convert bytes32 log topic to an address
+function bytesToAddress(data: Bytes): Address {
+  // Extract the last 20 bytes (20 bytes address from the 32 bytes data)
+  let addressBytes = data.subarray(12); // Skip the first 12 bytes (24 hex characters)
+  return Address.fromBytes(Bytes.fromUint8Array(addressBytes));
+}
 
 export function handleWithdraw(event: WithdrawEvent): void {
-  let entity = new Withdraw(
+  let withdraw = new Withdraw(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._secret = event.params._secret
-  entity.addr = event.params.addr
-  entity.amount = event.params.amount
+  );
+  withdraw.secret = event.params._secret;
+  withdraw.address = event.params.addr;
+  withdraw.amount = event.params.amount;
+  withdraw.timestamp = event.block.timestamp;
+  withdraw.save();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // handle transaction logs with erc20 transfers
+  const logs = event.receipt!.logs;
+  for (let i = 0; i < logs.length; i++) {
+    let logData = logs[i];
+    if (logData.topics[0].toHex() == TRANSFER_EVENT_SIGNATURE) {
+      // Decode the log data with the Transfer event signature
+      const fromAddress = ethereum
+        .decode("address", logData.topics[1])!
+        .toAddress();
+      const toAddress = ethereum
+        .decode("address", logData.topics[2])!
+        .toAddress();
+      const transferAmount = ethereum
+        .decode("uint256", logData.data)!
+        .toBigInt();
 
-  entity.save()
+      log.debug("logData from transaction: {}, {}, {}, {}", [
+        logData.address.toHex(),
+        fromAddress.toHex(),
+        toAddress.toHex(),
+        transferAmount.toString()
+      ]);
+
+      // Decode the log data with the Transfer event signature
+      const transfer = new Transfer(
+        event.transaction.hash.concatI32(event.logIndex.toI32())
+      );
+      transfer.token = logData.address;
+      // topic1 is the from address it should be 20 bytes
+      transfer.from = fromAddress;
+      transfer.to = toAddress;
+      transfer.value = transferAmount;
+      transfer.txHash = event.transaction.hash;
+      transfer.timestamp = event.block.timestamp;
+      transfer.save();
+    }
+  }
 }
